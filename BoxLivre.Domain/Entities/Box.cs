0dc8915a964b9ef24939aa9d34994d6f609b1c9c
anderfalko <anderfalko@gmail.com>
2016-04-08 21:07:10 -0300

    Primeiro commit - só pra ver como é.

---
+++ BoxLivre.Domain/Entities/Box.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace BoxLivre.Domain.Entities
+{
+    public class Box : EntityBase
+    {
+
+    }
+}
---
+++ BoxLivre.Domain/Entities/EntityBase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BoxLivre.Domain.Entities
+{
+    public class EntityBase
+    {
+        public int UsuarioCriacao { get; set; }
+        public DateTime DataHoraCriacao { get; set; }
+        public int UsuarioEdicao { get; set; }
+        public DateTime DataHoraEdicao { get; set; }
+    }
+}
